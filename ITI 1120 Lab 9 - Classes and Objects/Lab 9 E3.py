2df20f1e028088bff755ce70925212d377db1fea
Soorya Saravanapavan <suri111200@gmail.com>
2018-11-30 15:07:14 -0500

    Incorporated a derived class

--- ITI 1120 Lab 9 - Classes and Objects/Lab 9 E3.py
+++ ITI 1120 Lab 9 - Classes and Objects/Lab 9 E3.py
@@ -1,3 +1,4 @@
+#Experimenting with derived classes and inheritance
 class BankAccount:
     def __init__(self,name='Dupont',solde=1000):
         self.name = name
@@ -11,3 +12,19 @@ class BankAccount:
 
     def display(self):
         print("The solde of the Bank account of",self.name,"is",self.solde,"dollars.")
+
+class AccountSaving(BankAccount):
+    def __init__(self, name='Dupont',solde = 1000):
+        BankAccount.__init__(self,name='Dupont',solde=1000)
+        self.rate= 0.003
+        self.name = name
+        self.solde = solde
+
+    def changeRate(self,value=0.003):
+            self.rate = value
+
+    def Capitalisation(self,Month):
+        print("Capitalisation of", Month,"at the monthly rate of",self.rate*100,"%.")
+        for i in range(Month-1):
+            self.solde = self.solde*(1+self.rate)
+    
